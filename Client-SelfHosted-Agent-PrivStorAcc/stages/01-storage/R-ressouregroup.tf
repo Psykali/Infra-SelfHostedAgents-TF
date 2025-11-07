aa1a2e442ae94be474bd890a8384d78a284322de
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 16:44:10 +0000

    Updated R-ressouregroup.tf

--- Client-SelfHosted-Agent-PrivStorAcc/stages/01-storage/R-ressouregroup.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/01-storage/R-ressouregroup.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "storage" {
   name     = "BSE-${var.client_name}-RG-${var.location}-001"
   location = var.location
   tags = {
-    Environment = var.environment
+    Environment = "prod"
     Client      = var.client_name
   }
 }
