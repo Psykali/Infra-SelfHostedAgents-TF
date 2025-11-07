8615dbd51e1e119d82fef7f0e532b1692eac146a
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 17:46:20 +0000

    Updated outputs.tf

--- Client-SelfHosted-Agent-PrivStorAcc/stages/03-ado-agent/outputs.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/03-ado-agent/outputs.tf
@@ -5,3 +5,11 @@ output "agent_pool_name" {
 output "agent_pool_id" {
   value = azuredevops_agent_pool.client_pool.id
 }
+
+output "project_id" {
+  value = azuredevops_project.main.id
+}
+
+output "organization_url" {
+  value = "https://dev.azure.com/${var.ado_organization}"
+}
