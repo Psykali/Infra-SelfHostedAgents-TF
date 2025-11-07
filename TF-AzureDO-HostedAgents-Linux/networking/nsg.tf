f1b7dc9439a1dd9f813d7da541d9e598f126b500
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 10:46:17 +0000

    Updated nsg.tf

--- TF-AzureDO-HostedAgents-Linux/networking/nsg.tf
+++ TF-AzureDO-HostedAgents-Linux/networking/nsg.tf
@@ -21,6 +21,48 @@ resource "azurerm_network_security_rule" "ssh" {
   network_security_group_name = azurerm_network_security_group.vm.name
 }
 
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "AllowRDP"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.spoke_networking.name
+  network_security_group_name = azurerm_network_security_group.vm.name
+}
+
+resource "azurerm_network_security_rule" "http" {
+  name                        = "AllowHTTP"
+  priority                    = 1003
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.spoke_networking.name
+  network_security_group_name = azurerm_network_security_group.vm.name
+}
+
+resource "azurerm_network_security_rule" "https" {
+  name                        = "AllowHTTPS"
+  priority                    = 1004
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.spoke_networking.name
+  network_security_group_name = azurerm_network_security_group.vm.name
+}
+
 resource "azurerm_subnet_network_security_group_association" "vm" {
   subnet_id                 = azurerm_subnet.vm.id
   network_security_group_id = azurerm_network_security_group.vm.id
