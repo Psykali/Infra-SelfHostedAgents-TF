c73b6ed52669d471dfdd8450733f565db84b8ab7
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 16:44:25 +0000

    Updated outputs.tf

--- Client-SelfHosted-Agent-PrivStorAcc/stages/01-storage/outputs.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/01-storage/outputs.tf
@@ -2,14 +2,10 @@ output "storage_account_name" {
   value = azurerm_storage_account.tfstate.name
 }
 
-output "storage_container_name" {
-  value = azurerm_storage_container.tfstate.name
+output "storage_account_id" {
+  value = azurerm_storage_account.tfstate.id
 }
 
 output "resource_group_name" {
   value = azurerm_resource_group.storage.name
-}
-
-output "private_endpoint_id" {
-  value = azurerm_private_endpoint.storage.id
 }
