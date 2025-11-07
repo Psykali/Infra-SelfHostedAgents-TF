810228d3f4e803256bcfa2b895c37dc6efa0b6ee
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 16:46:25 +0000

    Updated R-private_endpoint_vm.tf

--- Client-SelfHosted-Agent-PrivStorAcc/stages/02-networking-vm/R-private_endpoint_vm.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/02-networking-vm/R-private_endpoint_vm.tf
@@ -1,11 +1,11 @@
-resource "azurerm_private_endpoint" "storage_from_vm" {
-  name                = "BSE-${var.client_name}-PE-VM-${var.location}-001"
+resource "azurerm_private_endpoint" "storage" {
+  name                = "BSE-${var.client_name}-PE-${var.location}-001"
   resource_group_name = azurerm_resource_group.network.name
   location            = var.location
   subnet_id           = azurerm_subnet.private_endpoints.id
 
   private_service_connection {
-    name                           = "BSE-${var.client_name}-PSC-VM-${var.location}-001"
+    name                           = "BSE-${var.client_name}-PSC-${var.location}-001"
     private_connection_resource_id = var.storage_account_id
     is_manual_connection           = false
     subresource_names              = ["blob"]
