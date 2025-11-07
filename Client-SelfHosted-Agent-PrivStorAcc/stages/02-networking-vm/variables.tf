b3834234e444d7c8648b0710a3ead8690f04cb38
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 16:17:14 +0000

    Added variables.tf

---
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/02-networking-vm/variables.tf
@@ -0,0 +1,33 @@
+variable "client_name" {
+  description = "Name of the client"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "France Central"
+}
+
+variable "admin_username" {
+  description = "Admin username for VM"
+  type        = string
+  default     = "BseSelfAgent"
+}
+
+variable "admin_password" {
+  description = "Admin password for VM"
+  type        = string
+  sensitive   = true
+}
+
+variable "vm_size" {
+  description = "VM size"
+  type        = string
+  default     = "Standard_B2s"
+}
+
+variable "storage_account_name" {
+  description = "Storage account name for private endpoint"
+  type        = string
+}
