f15058a8ca6d4b9605ab830213c212a48423de16
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 16:50:30 +0000

    Added providers.tf

---
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/05-test-resources/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "BSE-${var.client_name}-RG-${var.location}-001"
+    storage_account_name = var.storage_account_name
+    container_name       = "tfstate"
+    key                  = "stage5-test.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
