409d069eea71d6b9703eb659f03273b280929266
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 17:39:29 +0000

    Updated test.tf

--- Client-SelfHosted-Agent-PrivStorAcc/stages/05-test-resources/test.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/05-test-resources/test.tf
@@ -1,3 +1,8 @@
+data "azurerm_storage_account" "tfstate" {
+  name                = "bse${var.client_name}st${var.location}001"
+  resource_group_name = "BSE-${var.client_name}-RG-${var.location}-001"
+}
+
 resource "azurerm_resource_group" "test" {
   name     = "BSE-${var.client_name}-RG-TEST-${var.location}-001"
   location = var.location
@@ -9,13 +14,13 @@ resource "azurerm_resource_group" "test" {
 
 resource "azurerm_storage_container" "test" {
   name                  = "test-container"
-  storage_account_name  = var.storage_account_name
+  storage_account_name  = data.azurerm_storage_account.tfstate.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "test" {
   name                   = "test-file.txt"
-  storage_account_name   = var.storage_account_name
+  storage_account_name   = data.azurerm_storage_account.tfstate.name
   storage_container_name = azurerm_storage_container.test.name
   type                   = "Block"
   source_content         = "This is a test file created via private endpoint connection"
