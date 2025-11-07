fef4af3f748521e3830233bcde8bb01b31ba4e3f
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 17:45:13 +0000

    Updated variables.tf

--- Client-SelfHosted-Agent-PrivStorAcc/stages/03-ado-agent/variables.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/03-ado-agent/variables.tf
@@ -1,9 +1,3 @@
-variable "client_name" {
-  description = "Name of the client"
-  type        = string
-  default     = "client"
-} 
-
 variable "ado_organization" {
   description = "Azure DevOps organization name"
   type        = string
@@ -14,4 +8,16 @@ variable "ado_project" {
   description = "Azure DevOps project name"
   type        = string
   default     = "TestScripts-Forge"
+}
+
+variable "client_name" {
+  description = "Name of the client"
+  type        = string
+  default     = "client"
+}
+
+variable "devops_admin_pat" {
+  description = "Azure DevOps PAT with sufficient permissions to create resources"
+  type        = string
+  sensitive   = true
 }
