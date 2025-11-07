913bc27863d6f29846cd4b9f5944f651de05a73a
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 17:31:54 +0000

    Updated variables.tf

--- Client-SelfHosted-Agent-PrivStorAcc/stages/03-ado-agent/variables.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/03-ado-agent/variables.tf
@@ -1,14 +1,17 @@
 variable "client_name" {
   description = "Name of the client"
   type        = string
-}
+  default     = "client"
+} 
 
 variable "ado_organization" {
   description = "Azure DevOps organization name"
   type        = string
+  default     = "bseforgedevops"
 }
 
 variable "ado_project" {
   description = "Azure DevOps project name"
   type        = string
+  default     = "TestScripts-Forge"
 }
