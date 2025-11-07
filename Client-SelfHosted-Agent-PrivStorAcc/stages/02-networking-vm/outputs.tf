6ca66e953b9403843a5d18b953217ff2228ecf6a
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 16:20:50 +0000

    Added outputs.tf

---
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/02-networking-vm/outputs.tf
@@ -0,0 +1,11 @@
+output "vm_public_ip" {
+  value = azurerm_public_ip.vm.ip_address
+}
+
+output "vm_private_ip" {
+  value = azurerm_network_interface.vm.private_ip_address
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.private_endpoints.id
+}
