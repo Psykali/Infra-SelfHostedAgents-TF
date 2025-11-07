5a98fa72feae7d700cfa696541cac7a5c3bb674a
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 17:28:16 +0000

    Updated variables.tf

--- Client-SelfHosted-Agent-PrivStorAcc/stages/01-storage/variables.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/01-storage/variables.tf
@@ -1,11 +1,11 @@
 variable "client_name" {
   description = "Name of the client"
   type        = string
-#  default     = "client"
+  default     = "client"
 }
 
 variable "location" {
   description = "Azure region"
   type        = string
-#  default     = "France Central"
+  default     = "France Central"
 }
