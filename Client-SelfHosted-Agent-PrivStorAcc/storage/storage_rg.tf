b00d0b5e20cdbe7ff54bf6d4bced0e88eb05e037
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 14:33:07 +0000

    Deleted storage

--- Client-SelfHosted-Agent-PrivStorAcc/storage/storage_rg.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "storage" {
-  name     = "rg-storage-${var.location}"
-  location = var.location
-  tags = {
-    Environment = "Production"
-    Component   = "Storage"
-    Purpose     = "Terraform-State"
-  }
-}
