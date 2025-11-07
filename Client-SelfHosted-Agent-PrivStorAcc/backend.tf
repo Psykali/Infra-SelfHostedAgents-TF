e794d5601dd624d79f05fbf6bea3fd06e6ad7e91
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 16:06:00 +0000

    Deleted Client-SelfHosted-Agent-PrivStorAcc

--- Client-SelfHosted-Agent-PrivStorAcc/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-state-storage"
-    storage_account_name = "statestoragetfdevops"
-    container_name       = "tfstate"
-    key                  = "devops-agents.tfstate"
-  }
-}
