a55b85c751887f325a76a30874967216f13af3c1
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 14:29:08 +0000

    Updated variables.tf

--- Client-SelfHosted-Agent-PrivStorAcc/variables.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/variables.tf
@@ -4,40 +4,10 @@ variable "location" {
   default     = "France Central"
 }
 
-variable "hub_vnet_cidr" {
-  description = "CIDR block for Hub VNet"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "spoke_vnet_cidr" {
-  description = "CIDR block for Spoke VNet"
-  type        = string
-  default     = "10.1.0.0/16"
-}
-
-variable "vm_subnet_cidr" {
-  description = "CIDR block for VM subnet"
-  type        = string
-  default     = "10.1.1.0/24"
-}
-
-variable "private_endpoint_subnet_cidr" {
-  description = "CIDR block for private endpoint subnet"
-  type        = string
-  default     = "10.1.2.0/24"
-}
-
-variable "ubuntu_vm_size" {
-  description = "Size of the Ubuntu virtual machine"
-  type        = string
-  default     = "Standard_B2s"
-}
-
 variable "admin_username" {
   description = "Admin username for the VM"
   type        = string
-  default     = "azureuser"
+  default     = "bseado-selfhostedagent"
 }
 
 variable "admin_password" {
@@ -63,14 +33,14 @@ variable "devops_agent_pool" {
   default     = "Default"
 }
 
-variable "ubuntu_agent_name" {
-  description = "Name for the Ubuntu self-hosted agent"
+variable "devops_agent_name" {
+  description = "Name for the self-hosted agent"
   type        = string
   default     = "ubuntu-agent-01"
 }
 
 variable "storage_account_name" {
-  description = "Name for the storage account (must be unique)"
+  description = "Name of the storage account for Terraform state"
   type        = string
-  default     = "statetfdevopsfrc01"
+  default     = "statestoragetfdevops"
 }
