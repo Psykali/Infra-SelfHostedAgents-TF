fe8a52a1371687cd097ac429180977b95c07dd68
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 16:24:42 +0000

    Added variables.tf

---
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/03-ado-agent/variables.tf
@@ -0,0 +1,14 @@
+variable "client_name" {
+  description = "Name of the client"
+  type        = string
+}
+
+variable "ado_organization" {
+  description = "Azure DevOps organization name"
+  type        = string
+}
+
+variable "ado_project" {
+  description = "Azure DevOps project name"
+  type        = string
+}
