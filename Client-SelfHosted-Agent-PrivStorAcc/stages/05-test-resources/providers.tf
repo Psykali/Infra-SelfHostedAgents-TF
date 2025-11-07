55344342b77a41f5af8c7bf36ace371d5a0e343c
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-07 18:35:34 +0000

    Updated providers.tf

--- Client-SelfHosted-Agent-PrivStorAcc/stages/05-test-resources/providers.tf
+++ Client-SelfHosted-Agent-PrivStorAcc/stages/05-test-resources/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "BSE-${var.client_name}-RG-${var.location}-001"
-    storage_account_name = var.storage_account_name
+    storage_account_name = "bseclientstfr001"
     container_name       = "tfstate"
     key                  = "stage5-test.tfstate"
   }
