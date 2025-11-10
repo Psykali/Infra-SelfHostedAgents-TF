91483098e67d56a2f5453dceb984588036aa57b8
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-10 13:20:51 +0000

    Updated variables.tf

--- R-SelfHostedAgents_LinuxVM/variables.tf
+++ R-SelfHostedAgents_LinuxVM/variables.tf
@@ -23,32 +23,6 @@ variable "vm_size" {
   default     = "Standard_B2als_v2"
 }
 
-variable "agent_count" {
-  description = "Number of DevOps agents to install"
-  default     = 5
-}
-
-variable "devops_org" {
-  description = "Azure DevOps organization"
-  default = "bseforgedevops"
-}
-
-variable "devops_project" {
-  description = "Azure DevOps Project"
-  default = "TestScripts-Forge"
-}
-
-variable "devops_pool" {
-  description = "Azure DevOps agent pool name"
-  default     = "client-hostedagents-ubuntu01"
-}
-
-variable "devops_pat" {
-  description = "Azure DevOps Personal Access Token Named: client-devops-pat"
-  sensitive   = true
-  default     = "BSAAkacP3YMqphCwk0jwyYuYyZMW4QYe3tOVdbCHpEVXAcO8up4XJQQJ99BKACAAAAA2O8gkAAASAZDOgQ7J"
-}
-
 variable "nsg_name" {
   description  = " NSG Name "
   default      = "client-devops-agent-nsg"
