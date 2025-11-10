92c22b2679d09a3a384a28e3c3a4551593df2541
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-10 13:10:22 +0000

    Updated main.tf

--- R-SelfHostedAgents_LinuxVM/main.tf
+++ R-SelfHostedAgents_LinuxVM/main.tf
@@ -110,15 +110,3 @@ resource "azurerm_linux_virtual_machine" "main" {
     type = "SystemAssigned"
   }
 }
-
-output "vm_public_ip" {
-  value = azurerm_public_ip.main.ip_address
-}
-
-output "vm_resource_group_name" {
-  value = azurerm_resource_group.vm_rg.name
-}
-
-output "networking_resource_group_name" {  
-  value = azurerm_resource_group.network_rg.name
-}
