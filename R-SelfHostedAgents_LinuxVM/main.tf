26ab1155597ff648308cd687f9b3bacd93274165
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-10 13:12:57 +0000

    Updated main.tf

--- R-SelfHostedAgents_LinuxVM/main.tf
+++ R-SelfHostedAgents_LinuxVM/main.tf
@@ -1,14 +1,3 @@
-# Resource Group
-resource "azurerm_resource_group" "vm_rg" {
-  name     = var.vm_rg_name
-  location = var.location
-}
-
-resource "azurerm_resource_group" "network_rg" {
-  name     = var.networking_rg_name
-  location = var.location
-}
-
 # Network Security Group
 resource "azurerm_network_security_group" "main" {
   name                = var.nsg_name
@@ -70,43 +59,4 @@ resource "azurerm_network_interface" "main" {
 resource "azurerm_network_interface_security_group_association" "main" {
   network_interface_id      = azurerm_network_interface.main.id 
   network_security_group_id = azurerm_network_security_group.main.id  
-}
-
-# Linux Virtual Machine
-resource "azurerm_linux_virtual_machine" "main" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.vm_rg.name
-  location            = azurerm_resource_group.vm_rg.location
-  size                = var.vm_size
-  admin_username      = "devopsadmin"
-  admin_password      = "FGHJfghj1234"
-  disable_password_authentication = false  
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  # Custom script to install and configure DevOps agents
-  custom_data = base64encode(templatefile("${path.module}/agent-setup.sh", {
-    devops_org    = var.devops_org
-    devops_project = var.devops_project  
-    devops_pool   = var.devops_pool
-    devops_pat    = var.devops_pat
-    agent_count   = var.agent_count
-  }))
-
-  identity {
-    type = "SystemAssigned"
-  }
 }
