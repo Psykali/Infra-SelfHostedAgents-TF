1624df6cc2ed69984e4dd2b3ace68128f9045b3a
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-10 10:09:51 +0000

    Deleted Client-SelfHosted-Agent-PrivStorAcc

--- Client-SelfHosted-Agent-PrivStorAcc/stages/01-storage/R-ressouregroup.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "storage" {
-  name     = "BSE-${var.client_name}-RG-001"
-  location = var.location
-  tags = {
-    Environment = "prod"
-    Client      = var.client_name
-  }
-}
--- Client-SelfHosted-Agent-PrivStorAcc/stages/05-test-resources/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "test_blob_url" {
-  value = azurerm_storage_blob.test.url
-}
-
-output "test_resource_group" {
-  value = azurerm_resource_group.test.name
-}
