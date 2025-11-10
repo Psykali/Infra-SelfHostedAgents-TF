756c054b6ceaffcdb03be06e01bc9f98b38702a5
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-10 13:59:51 +0000

    Updated R-RessourceGroups.tf

--- R-SelfHostedAgents_LinuxVM/R-RessourceGroups.tf
+++ R-SelfHostedAgents_LinuxVM/R-RessourceGroups.tf
@@ -1,10 +1,19 @@
-# Resource Group
 resource "azurerm_resource_group" "vm_rg" {
   name     = var.vm_rg_name
   location = var.location
+
+  tags = merge(local.common_tags, {
+    Description = "Resource group for DevOps agent VMs"
+    Component   = "compute"
+  })
 }
 
 resource "azurerm_resource_group" "network_rg" {
   name     = var.networking_rg_name
   location = var.location
+
+  tags = merge(local.common_tags, {
+    Description = "Resource group for networking resources"
+    Component   = "networking"
+  })
 }
