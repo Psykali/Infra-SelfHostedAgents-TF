649df4dba9ecc85208e93abc4f5d2a6793d8b64d
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-10 10:09:46 +0000

    Deleted TF-AzureDO-HostedAgents-Linux

--- TF-AzureDO-HostedAgents-Linux/networking/hub_rg.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "hub_networking" {
-  name     = "rg-hub-networking-${var.location}"
-  location = var.location
-  tags = {
-    Environment = "Production"
-    Component   = "Networking"
-  }
-}
--- TF-AzureDO-HostedAgents-Linux/networking/spoke_rg.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "spoke_networking" {
-  name     = "rg-spoke-networking-${var.location}"
-  location = var.location
-  tags = {
-    Environment = "Production"
-    Component   = "Networking"
-  }
-}
