ddead4436ab56605210abc87178b96d774a51eeb
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-10 10:38:22 +0000

    Updated main.tf

--- R-SelfHostedAgents_LinuxVM/main.tf
+++ R-SelfHostedAgents_LinuxVM/main.tf
@@ -13,9 +13,13 @@ provider "azurerm" {
 }
 
 # Variables
-variable "resource_group_name" {
+variable "vm_rg_name" {
   description = "Name of the resource group"
-  default     = "client-devops-agents-rg"
+  default     = "client-devops-agents-vm-rg"
+}
+variable "networking_rg_name" {
+  description = "Name of the resource group"
+  default     = "client-devops-agents-network-rg"
 }
 
 variable "location" {
@@ -54,17 +58,47 @@ variable "devops_pat" {
   default     = "BSAAkacP3YMqphCwk0jwyYuYyZMW4QYe3tOVdbCHpEVXAcO8up4XJQQJ99BKACAAAAA2O8gkAAASAZDOgQ7J"
 }
 
+variable "nsg_name" {
+  description  = " NSG Name "
+  default      = "client-devops-agent-nsg"
+}
+
+variable "vnet_name" {
+  description = "Virtual Network Name"
+  default     = "client-devops-agent-vnet"
+}
+
+variable "subnet_name" {
+  description = "Subnet Name"
+  default     = "client-devops-agent-subnet"
+}
+
+variable "pip_name" {
+  description = "Public IP Name"
+  default     = "client-devops-agent-pip"
+}
+
+variable "nic_name" {
+  description = "Public IP Name"
+  default     = "client-devops-agent-nic"
+}
+
 # Resource Group
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
+resource "azurerm_resource_group" "vm_main" {
+  name     = var.vm_rg_name
+  location = var.location
+}
+
+resource "azurerm_resource_group" "networking_main" {
+  name     = var.networking_rg_name
   location = var.location
 }
 
 # Network Security Group
 resource "azurerm_network_security_group" "main" {
-  name                = "devops-agent-nsg"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  name                = var.nsg_name
+  location            = azurerm_resource_group.networking_main.location
+  resource_group_name = azurerm_resource_group.networking_main.name
 
   security_rule {
     name                       = "SSH"
@@ -81,37 +115,37 @@ resource "azurerm_network_security_group" "main" {
 
 # Virtual Network
 resource "azurerm_virtual_network" "main" {
-  name                = "devops-agent-vnet"
+  name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.networking_main.location
+  resource_group_name = azurerm_resource_group.networking_main.name
 }
 
 # Subnet
 resource "azurerm_subnet" "main" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.networking_main.name
+  virtual_network_name = azurerm_virtual_network.networking_main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Public IP
 resource "azurerm_public_ip" "main" {
-  name                = "devops-agent-pip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  name                = var.pip_name
+  location            = azurerm_resource_group.networking_main.location
+  resource_group_name = azurerm_resource_group.networking_main.name
   allocation_method   = "Dynamic"
 }
 
 # Network Interface
 resource "azurerm_network_interface" "main" {
-  name                = "devops-agent-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  name                = var.nic_name
+  location            = azurerm_resource_group.networking_main.location
+  resource_group_name = azurerm_resource_group.networking_main.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.main.id
+    subnet_id                     = azurerm_subnet.networking_main.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
   }
@@ -119,15 +153,15 @@ resource "azurerm_network_interface" "main" {
 
 # Connect NSG to NIC
 resource "azurerm_network_interface_security_group_association" "main" {
-  network_interface_id      = azurerm_network_interface.main.id
-  network_security_group_id = azurerm_network_security_group.main.id
+  network_interface_id      = azurerm_network_interface.networking_main.id
+  network_security_group_id = azurerm_network_security_group.networking_main.id
 }
 
 # Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "main" {
   name                = var.vm_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.vm_main.name
+  location            = azurerm_resource_group.vm_main.location
   size                = var.vm_size
   admin_username      = "devopsadmin"
   network_interface_ids = [
@@ -165,9 +199,13 @@ resource "azurerm_linux_virtual_machine" "main" {
 }
 
 output "vm_public_ip" {
-  value = azurerm_public_ip.main.ip_address
+  value = azurerm_public_ip.networking_main.ip_address
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.main.name
+output "vm_resource_group_name" {
+  value = azurerm_resource_group.vm_main.name
+}
+
+output "netowrking_resource_group_name" {
+  value = azurerm_resource_group.networking_main.name
 }
