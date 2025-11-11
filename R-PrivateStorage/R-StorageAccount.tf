1f8c6e22fede679bbaa044cedff4d8398e451bae
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-11 15:33:26 +0000

    Updated R-StorageAccount.tf

--- R-PrivateStorage/R-StorageAccount.tf
+++ R-PrivateStorage/R-StorageAccount.tf
@@ -1,3 +1,9 @@
+variable "allowed_ip" {
+  description = "Your allowed IP address"
+  default     = "176.147.43.231"
+}
+
+# Storage Account with public access enabled (for firewall rules to work)
 resource "azurerm_storage_account" "private" {
   name                     = var.private_storage_name
   resource_group_name      = azurerm_resource_group.storage.name
@@ -5,14 +11,28 @@ resource "azurerm_storage_account" "private" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
-
-  # Enable private network access
-  public_network_access_enabled = deny
-
+  public_network_access_enabled = true
   tags = {
     environment = "devops"
   }
 }
+
+resource "azurerm_storage_account_network_rules" "private" {
+  storage_account_id = azurerm_storage_account.private.id
+  
+  # DENY all traffic by default
+  default_action = "Deny"
+  
+  # Allow only your specific IP
+  ip_rules = [var.allowed_ip]
+  
+  # Allow private endpoint subnet
+  virtual_network_subnet_ids = [azurerm_subnet.private_endpoint.id]
+  
+  # Allow Azure services (required for private endpoints and other Azure services)
+  bypass = ["AzureServices"]
+}
+
 
 resource "null_resource" "wait_for_private_endpoint" {
   depends_on = [azurerm_private_endpoint.storage]
