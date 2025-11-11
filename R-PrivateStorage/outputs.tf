80f31a77fd9401c6a0ebebb9fa06276d40d3f0ef
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-11 12:44:45 +0000

    Added outputs.tf

---
+++ R-PrivateStorage/outputs.tf
@@ -0,0 +1,11 @@
+output "storage_account_name" {
+  value = azurerm_storage_account.private.name
+}
+
+output "storage_container_name" {
+  value = azurerm_storage_container.tfstate.name
+}
+
+output "storage_account_id" {
+  value = azurerm_storage_account.private.id
+}
