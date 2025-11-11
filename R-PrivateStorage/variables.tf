f53fe495f5a5653b6f9e66499143a32bb54a7bdd
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-11-11 12:46:29 +0000

    Added variables.tf

---
+++ R-PrivateStorage/variables.tf
@@ -0,0 +1,35 @@
+# Variables
+variable "private_storage_name" {
+  description = "Name of the storage account"
+  default     = "clienttfprivstacc"
+}
+
+variable "storage_rg_name" {
+  description = "Name of the resource group for storage"
+  default     = "rg-client-tf-storage"
+}
+
+variable "location" {
+  description = "Azure region"
+  default     = "francecentral"
+}
+
+variable "tfstate_container_name" {
+  description = "tfstate_container_name"
+  default     = "client_tfstate"
+}
+
+variable "private_endpoint_name" {
+  description = "Private Endpoint Name"
+  default     = "client-storage-private-endpoint"
+}
+
+variable "private_endpoint_connection_name" {
+  description = "Private Endpoint Connection Name"
+  default     = "client-storage-private-connection"
+}
+
+variable "private_endpoint_subnet_name" {
+  description = "Name of the subnet for private endpoint"
+  default     = "private-endpoint-subnet"
+}
