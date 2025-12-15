a3ff310821baff7651baee3d5b00b04788a8c15a
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 09:31:00 +0000

    Updated variables.tf

--- R-PrivateStorage/variables.tf
+++ R-PrivateStorage/variables.tf
@@ -1,35 +1,26 @@
-# Variables
-variable "private_storage_name" {
-  description = "Name of the storage account"
-  default     = "clienttfprivstacc"
+### ----------------------------------------------
+### Naming Should be Homogene with the Azure & Vlient naming Policies
+### ----------------------------------------------
+variable "customer" {
+  description = "Customer short-code (2-5 lower-case letters/numbers)"
+  type        = string
+  default     = "client"   # <-- change only this line
 }
 
-variable "storage_rg_name" {
-  description = "Name of the resource group for storage"
-  default     = "client-tfstate-storage-rg"
-}
+locals {
+  base_storage = "tfstate"          # workload suffix for storage assets
 
-variable "location" {
-  description = "Azure region"
-  default     = "francecentral"
-}
+  # Storage resource-group name
+  storage_rg_name = "rg-storage-${var.customer}-${local.base_storage}"
 
-variable "tfstate_container_name" {
-  description = "tfstate_container_name"
-  default     = "client-tfstate"
-}
+  # Storage account (24-char max, lowercase, no hyphens at ends)
+  private_storage_name = "${var.customer}st${local.base_storage}001"
 
-variable "private_endpoint_name" {
-  description = "Private Endpoint Name"
-  default     = "client-storage-private-endpoint"
-}
+  # Container names (lower-case, numbers & hyphens only)
+  tfstate_container_name = "tfstate-${var.customer}"
 
-variable "private_endpoint_connection_name" {
-  description = "Private Endpoint Connection Name"
-  default     = "client-storage-private-connection"
-}
-
-variable "private_endpoint_subnet_name" {
-  description = "Name of the subnet for private endpoint"
-  default     = "private-endpoint-subnet"
+  # Private-endpoint related names
+  private_endpoint_name            = "pe-${var.customer}-${local.base_storage}-blob"
+  private_endpoint_connection_name = "pec-${var.customer}-${local.base_storage}-blob"
+  private_endpoint_subnet_name     = "snet-${var.customer}-${local.base_storage}-pe"
 }
