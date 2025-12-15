a352f2d1789f8a2b89a663d1ef295dc0c844fd37
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 13:05:36 +0000

    Updated R-Networking.tf

--- R-PrivateStorage/R-Networking.tf
+++ R-PrivateStorage/R-Networking.tf
@@ -5,7 +5,7 @@ resource "azurerm_subnet" "private_endpoint" {
   name                 = local.private_endpoint_subnet_name
   resource_group_name  = local.networking_rg_name
   virtual_network_name = local.vnet_name
-  address_prefixes     = ["10.0.0.33/27"]
+  address_prefixes     = ["10.0.0.32/27"]
 
   service_endpoints = ["Microsoft.Storage"]
   
