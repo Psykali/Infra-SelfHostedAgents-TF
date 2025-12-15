bda0fc2859e6d813ff726806603e8b888b041c1f
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 09:47:24 +0000

    Updated R-Networking.tf

--- R-PrivateStorage/R-Networking.tf
+++ R-PrivateStorage/R-Networking.tf
@@ -1,26 +1,26 @@
 ### ------------------------------------------------
-### Create a dedicated subnet for private endpoints
+### Dedicated subnet for private endpoints (no DNS)
 ### ------------------------------------------------
 resource "azurerm_subnet" "private_endpoint" {
   name                 = local.private_endpoint_subnet_name
-  resource_group_name  = "client-devops-agents-network-rg"  
+  resource_group_name  = azurerm_resource_group.storage.name
   virtual_network_name = data.azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.3.0/24"]  # Different subnet than the VM subnet
-  
+  address_prefixes     = ["10.0.3.0/24"]
+
   service_endpoints = ["Microsoft.Storage"]
 
+  # Allow private-endpoint policies to be applied
   private_endpoint_network_policies = "Disabled"
-  
 }
 
 ### -------------------------
-### Create private endpoint
+### Private endpoint (no DNS zone)
 ### -------------------------
 resource "azurerm_private_endpoint" "storage" {
-  name                = local.private_endpoint_name
+  name                = var.private_endpoint_name
   location            = azurerm_resource_group.storage.location
   resource_group_name = azurerm_resource_group.storage.name
-  subnet_id           = azurerm_subnet.private_endpoint.id  
+  subnet_id           = azurerm_subnet.private_endpoint.id
 
   private_service_connection {
     name                           = var.private_endpoint_connection_name
@@ -28,27 +28,4 @@ resource "azurerm_private_endpoint" "storage" {
     subresource_names              = ["blob"]
     is_manual_connection           = false
   }
-
-  private_dns_zone_group {
-    name                 = "default"
-    private_dns_zone_ids = [azurerm_private_dns_zone.blob.id]
-  }
-}
-
-resource "azurerm_private_dns_zone" "blob" {
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = azurerm_resource_group.storage.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "storage" {
-  name                  = "storage-dns-link"
-  resource_group_name   = azurerm_resource_group.storage.name
-  private_dns_zone_name = azurerm_private_dns_zone.blob.name
-  virtual_network_id    = data.azurerm_virtual_network.main.id 
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-  upper   = false
 }
