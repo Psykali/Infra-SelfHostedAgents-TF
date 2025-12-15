643f98f990b25787b31937bf5d0361d3f30a77ea
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 10:00:50 +0000

    Updated variables-locals.tf

--- R-PrivateStorage/variables-locals.tf
+++ R-PrivateStorage/variables-locals.tf
@@ -4,10 +4,11 @@
 variable "customer" {
   description = "Customer short-code (2-5 lower-case letters/numbers)"
   type        = string
-  default     = "client"   # <-- change only this line
+  default     = "test"   # <-- change only this line
 }
 
 locals {
+  base = "ado-agents"               # workload suffix from stage 1 for data
   base_storage = "tfstate"          # workload suffix for storage assets
 
   # Storage resource-group name
@@ -23,6 +24,11 @@ locals {
   private_endpoint_name            = "pe-${var.customer}-${local.base_storage}-blob"
   private_endpoint_connection_name = "pec-${var.customer}-${local.base_storage}-blob"
   private_endpoint_subnet_name     = "snet-${var.customer}-${local.base_storage}-pe"
+
+  # network objects from stage 1 for data 
+  networking_rg_name= "rg-networking-${var.customer}-${local.base}"
+  vnet_name   = "vnet-${var.customer}-${local.base}"
+  subnet_name = "snet-${var.customer}-${local.base}"
 }
 
 ### -------------------------------------------------------------------
