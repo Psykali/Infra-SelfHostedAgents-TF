6b520527a43d28e4ef87fbe59018b7c5affadc11
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 08:46:13 +0000

    Added R-Vnet.tf

---
+++ R-SelfHostedAgents_LinuxVM/NetworkingModules/R-Vnet.tf
@@ -0,0 +1,22 @@
+### ----------------------
+### Virtual Network "VNET"
+### ----------------------
+resource "azurerm_virtual_network" "main" {
+  name                = local.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.network_rg.location
+  resource_group_name = azurerm_resource_group.network_rg.name
+
+  tags = merge(local.common_tags, {
+    Description = "Virtual network for DevOps infrastructure"
+    Component   = "networking"
+  })
+}
+
+# Subnet 
+resource "azurerm_subnet" "main" {
+  name                 = local.subnet_name
+  resource_group_name  = azurerm_resource_group.network_rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
