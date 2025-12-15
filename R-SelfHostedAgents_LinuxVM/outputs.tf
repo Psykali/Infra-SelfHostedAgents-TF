c9091e6dcedd2ad539ce1bc11e0178a7f10e3d31
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 09:33:38 +0000

    Updated outputs.tf

--- R-SelfHostedAgents_LinuxVM/outputs.tf
+++ R-SelfHostedAgents_LinuxVM/outputs.tf
@@ -1,3 +1,15 @@
+### -----
+### Data
+### -----
+data "azurerm_public_ip" "vm_ip" {
+  name                = azurerm_public_ip.main.name
+  resource_group_name = azurerm_resource_group.network_rg.name
+  depends_on = [azurerm_linux_virtual_machine.main]
+}
+
+### ---------
+### Outputs
+### ---------
 output "vm_public_ip" {
   value = azurerm_public_ip.main.ip_address
 }
