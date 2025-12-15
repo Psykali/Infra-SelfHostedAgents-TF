1804c85d799bc19c9a9d203132e9704abb6e4fd1
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 12:21:12 +0000

    Updated R-Vnet-Subnet.tf

--- R-SelfHostedAgents_LinuxVM/R-Vnet-Subnet.tf
+++ R-SelfHostedAgents_LinuxVM/R-Vnet-Subnet.tf
@@ -18,5 +18,5 @@ resource "azurerm_subnet" "main" {
   name                 = local.subnet_name
   resource_group_name  = azurerm_resource_group.network_rg.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.0.2/27"]
+  address_prefixes     = ["10.0.0.0/27"]
 }
