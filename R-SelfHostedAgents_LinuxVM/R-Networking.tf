fffd6ff8dddce0c3750d03e243357acb5f365519
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 08:42:45 +0000

    Updated R-Networking.tf

--- R-SelfHostedAgents_LinuxVM/R-Networking.tf
+++ R-SelfHostedAgents_LinuxVM/R-Networking.tf
@@ -1,6 +1,8 @@
-# Network Security Group "NSG"
+### ----------------------------
+### Network Security Group "NSG"
+### ----------------------------
 resource "azurerm_network_security_group" "main" {
-  name                = var.nsg_name
+  name                = local.nsg_name
   location            = azurerm_resource_group.network_rg.location
   resource_group_name = azurerm_resource_group.network_rg.name
 
@@ -21,10 +23,11 @@ resource "azurerm_network_security_group" "main" {
     Component   = "security"
   })
 }
-
-# Virtual Network "VNET"
+### ----------------------
+### Virtual Network "VNET"
+### ----------------------
 resource "azurerm_virtual_network" "main" {
-  name                = var.vnet_name
+  name                = local.vnet_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.network_rg.location
   resource_group_name = azurerm_resource_group.network_rg.name
@@ -37,15 +40,16 @@ resource "azurerm_virtual_network" "main" {
 
 # Subnet 
 resource "azurerm_subnet" "main" {
-  name                 = var.subnet_name
+  name                 = local.subnet_name
   resource_group_name  = azurerm_resource_group.network_rg.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
-
-# Public IP "PIP"
+### ----------------------
+### Public IP "PIP"
+### ----------------------
 resource "azurerm_public_ip" "main" {
-  name                = var.pip_name
+  name                = local.pip_name
   location            = azurerm_resource_group.network_rg.location
   resource_group_name = azurerm_resource_group.network_rg.name
   allocation_method   = "Dynamic"
@@ -56,10 +60,11 @@ resource "azurerm_public_ip" "main" {
     Ephemeral   = "true"  # Dynamic IPs can change
   })
 }
-
-# Network Interface "NIC"
+### ----------------------
+### Network Interface "NIC"
+### ----------------------
 resource "azurerm_network_interface" "main" {
-  name                = var.nic_name
+  name                = local.nic_name
   location            = azurerm_resource_group.network_rg.location
   resource_group_name = azurerm_resource_group.network_rg.name
 
