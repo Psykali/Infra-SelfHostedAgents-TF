0f14589121635edcd26366997462c7a64b4df7c1
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 09:34:38 +0000

    Updated outputs-datas.tf

--- R-PrivateStorage/outputs-datas.tf
+++ R-PrivateStorage/outputs-datas.tf
@@ -1,3 +1,21 @@
+### ---------------------------------------------------------
+### Data sources to reference existing networking resources
+### ---------------------------------------------------------
+
+data "azurerm_virtual_network" "main" {
+  name                = "client-devops-agent-vnet"  # Use actual VNet name from stage 1
+  resource_group_name = "client-devops-agents-network-rg" 
+}
+
+data "azurerm_subnet" "main" {
+  name                 = "client-devops-agent-subnet"  # Use actual subnet name from stage 1
+  virtual_network_name = data.azurerm_virtual_network.main.name
+  resource_group_name  = "client-devops-agents-network-rg" 
+}
+
+### --------
+### Outputs
+### --------
 output "storage_account_name" {
   value = azurerm_storage_account.private.name
 }
