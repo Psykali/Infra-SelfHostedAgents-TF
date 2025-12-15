db8a474730bad6de3fbfbc6c7364388c6f83b505
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 09:44:05 +0000

    Updated R-RessourceGroup.tf

--- R-PrivateStorage/R-RessourceGroup.tf
+++ R-PrivateStorage/R-RessourceGroup.tf
@@ -2,6 +2,6 @@
 ### Resource Group for Storage
 ### ---------------------------
 resource "azurerm_resource_group" "storage" {
-  name     = var.storage_rg_name
+  name     = local.storage_rg_name
   location = var.location
 }
