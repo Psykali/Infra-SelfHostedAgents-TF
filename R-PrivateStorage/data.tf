c00205df21e64e0dbec5b32bbdaa3c35d4d2f1e8
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 08:57:09 +0000

    Updated data.tf

--- R-PrivateStorage/data.tf
+++ R-PrivateStorage/data.tf
@@ -1,4 +1,7 @@
-# Data sources to reference existing networking resources
+### ---------------------------------------------------------
+### Data sources to reference existing networking resources
+### ---------------------------------------------------------
+
 data "azurerm_virtual_network" "main" {
   name                = "client-devops-agent-vnet"  # Use actual VNet name from stage 1
   resource_group_name = "client-devops-agents-network-rg" 
