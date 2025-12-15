fd7789a264f2b762a443329f55d35f40d882e14d
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 08:15:23 +0000

    Updated variables.tf

--- R-SelfHostedAgents_LinuxVM/variables.tf
+++ R-SelfHostedAgents_LinuxVM/variables.tf
@@ -1,31 +1,49 @@
+### ----------------------------------------------
 ### Variables to be modified to the clients values
 ### ----------------------------------------------
 variable "admin_username" {
-  description = "Name of the resource group"
+  description = "Name of the VM Admin login"
   sensitive = true
   default     = "devopsadmin"
 }
-
+### --------------------------------------------------------------------------------------------
 ### Password for the VM of the Agents, Create a strong Password and store it in bitwarden
+### --------------------------------------------------------------------------------------------
 variable "admin_password" {
   description = "Name of the resource group"
   sensitive = true
   default     = "FGHJfghj1234!"
 }
+### ----------------------------------------------
+### Naming Should be Homogene with the Azure & Vlient naming Policies
+### ----------------------------------------------
+variable "customer" {
+  description = "Customer short-code (2-6 lower-case letters/numbers)"
+  type        = string
+  default     = "acme"   # <-- change only this line
+}
+locals {
+  base = "ado-agents"               # fixed part describing the workload
+
+  # resource-group names
+  vm_rg_name        = "rg-${var.customer}-${local.base}"
+  networking_rg_name= "rg-${var.customer}-net"
+  # network objects
+  nsg_name    = "nsg-${var.customer}-${local.base}"
+  vnet_name   = "vnet-${var.customer}-${local.base}"
+  subnet_name = "snet-${var.customer}-${local.base}"
+
+  # VM objects (keep sequence number if you spin up several)
+  vm_name = "vm-${var.customer}-agt-01"
+  pip_name = "pip-${var.customer}-agt-01"
+  nic_name = "nic-${var.customer}-agt-01"
+}
+
+
 
 variable "vm_rg_name" {
   description = "Name of the resource group"
-  default     = "client-devops-agents-vm-rg"
-}
-
-variable "networking_rg_name" {
-  description = "Name of the resource group"
-  default     = "client-devops-agents-network-rg"
-}
-
-variable "location" {
-  description = "Azure region"
-  default     = "francecentral"
+  default     = "rg-ado-elfHosted-infa"
 }
 
 variable "vm_name" {
@@ -33,9 +51,9 @@ variable "vm_name" {
   default     = "client-devops-agent-vm"
 }
 
-variable "vm_size" {
-  description = "VM size"
-  default     = "Standard_B2als_v2"
+variable "networking_rg_name" {
+  description = "Name of the resource group"
+  default     = "client-devops-agents-network-rg"
 }
 
 variable "nsg_name" {
@@ -61,4 +79,16 @@ variable "pip_name" {
 variable "nic_name" {
   description = "Network Interface Name"
   default     = "client-devops-agent-nic"
+}
+### ----------------------------------------------
+### Location must be in France as a first option for the rules of RGPD
+### ----------------------------------------------
+variable "location" {
+  description = "Azure region"
+  default     = "francecentral"
+}
+
+variable "vm_size" {
+  description = "VM size"
+  default     = "Standard_B2als_v2"
 }
