b7da33f13977a9819e5afe5091b2f77c4f99949d
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2025-12-15 09:58:45 +0000

    Updated outputs-datas.tf

--- R-PrivateStorage/outputs-datas.tf
+++ R-PrivateStorage/outputs-datas.tf
@@ -1,23 +1,6 @@
 ### ---------------------------------------------------------
 ### Data sources to reference existing networking resources
 ### ---------------------------------------------------------
-### Variables for the data 
-### ----------------------
-variable "customer" {
-  description = "Customer short-code (2-5 lower-case letters/numbers)"
-  type        = string
-  default     = "test"   # <-- change only this line
-}
-locals {
-  base = "ado-agents"               # fixed part describing the workload
-
-  # resource-group names
-  networking_rg_name= "rg-networking-${var.customer}-${local.base}"
-  # network objects
-  vnet_name   = "vnet-${var.customer}-${local.base}"
-  subnet_name = "snet-${var.customer}-${local.base}"
-
-
 data "azurerm_virtual_network" "main" {
   name                = local.vnet_name 
   resource_group_name = local.networking_rg_name 
