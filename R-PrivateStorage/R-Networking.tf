cac536846c88ed0ac94105d324f6f08a7e630ec0
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-05 20:19:59 +0000

    Updated R-Networking.tf

--- R-PrivateStorage/R-Networking.tf
+++ R-PrivateStorage/R-Networking.tf
@@ -12,20 +12,3 @@ resource "azurerm_subnet" "private_endpoint" {
   # Allow private-endpoint policies to be applied
   private_endpoint_network_policies = "Enabled"
 }
-
-### -------------------------
-### Private endpoint (no DNS zone)
-### -------------------------
-resource "azurerm_private_endpoint" "storage" {
-  name                = local.private_endpoint_name
-  location            = azurerm_resource_group.storage.location
-  resource_group_name = azurerm_resource_group.storage.name
-  subnet_id           = azurerm_subnet.private_endpoint.id
-
-  private_service_connection {
-    name                           = local.private_endpoint_connection_name
-    private_connection_resource_id = azurerm_storage_account.private.id
-    subresource_names              = ["blob"]
-    is_manual_connection           = false
-  }
-}
