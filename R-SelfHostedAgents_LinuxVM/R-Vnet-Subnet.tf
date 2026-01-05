ff130fd2da88f65f42fd9c9e6a0524037310fede
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-05 15:32:39 +0000

    Updated R-Vnet-Subnet.tf

--- R-SelfHostedAgents_LinuxVM/R-Vnet-Subnet.tf
+++ R-SelfHostedAgents_LinuxVM/R-Vnet-Subnet.tf
@@ -7,7 +7,7 @@
 # Virtual Network
 resource "azurerm_virtual_network" "main" {
   name                = local.vnet_name
-  address_space       = ["10.0.0.0/24"]
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.network.location
   resource_group_name = azurerm_resource_group.network.name
   
@@ -22,10 +22,11 @@ resource "azurerm_subnet" "main" {
   name                 = local.subnet_name
   resource_group_name  = azurerm_resource_group.network.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.0.0/27"]
+  address_prefixes     = ["10.0.1.0/24"]
   
   # Enable private endpoints for future storage connectivity
-  private_endpoint_network_policies_enabled = true
+  # Updated to use new property name (non-deprecated)
+  private_endpoint_network_policies = "Enabled"
   
   # Service endpoints for Azure services
   service_endpoints = ["Microsoft.Storage"]
