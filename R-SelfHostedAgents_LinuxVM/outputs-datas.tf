bec4d91f7f0bd7dad453535eff6f0c26b589aea3
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-05 17:03:21 +0000

    Updated outputs-datas.tf

--- R-SelfHostedAgents_LinuxVM/outputs-datas.tf
+++ R-SelfHostedAgents_LinuxVM/outputs-datas.tf
@@ -2,7 +2,6 @@
 # DATA SOURCES AND OUTPUTS - DEVOPS AGENTS
 # =============================================
 # Purpose: Reference data and output deployment information
-# Usage: Gets VM public IP and outputs key information
 
 # Get VM public IP
 data "azurerm_public_ip" "vm_ip" {
@@ -33,44 +32,13 @@ output "key_vault_secret_name" {
   description = "Name of the secret storing VM password"
 }
 
-output "network_resource_group" {
-  value       = azurerm_resource_group.network.name
-  description = "Name of the network resource group"
+output "ssh_connection_command" {
+  value       = "ssh ${var.admin_username}@${data.azurerm_public_ip.vm_ip.ip_address}"
+  description = "SSH command to connect to VM"
 }
 
-output "agent_resource_group" {
-  value       = azurerm_resource_group.agent.name
-  description = "Name of the agent resource group"
-}
-
-output "vnet_name" {
-  value       = azurerm_virtual_network.main.name
-  description = "Name of the virtual network"
-}
-
-output "subnet_name" {
-  value       = azurerm_subnet.main.name
-  description = "Name of the subnet"
-}
-
-# FIXED: Added sensitive = true flag
-output "deployment_instructions" {
-  value = <<EOT
-✅ DevOps Agents Infrastructure Deployed!
-
-Next Steps:
-1. Retrieve VM password from Key Vault:
-   az keyvault secret show --vault-name ${azurerm_key_vault.main.name} --name vm-admin-password
-
-2. SSH to VM for verification:
-   ssh ${var.admin_username}@${data.azurerm_public_ip.vm_ip.ip_address}
-
-3. After storage deployment, update cloud-init.yaml with:
-   - Storage account private endpoint IP
-   - Storage account access key
-
-4. Run agent setup script on VM
-EOT
-  description = "Post-deployment instructions"
-  sensitive   = true  # ADDED THIS LINE
+output "key_vault_password_retrieval" {
+  value       = "az keyvault secret show --vault-name ${azurerm_key_vault.main.name} --name vm-admin-password --query value -o tsv"
+  description = "Command to retrieve VM password from Key Vault"
+  sensitive   = true
 }
