37fa7b0fc7a0e31bad25f7e371a72f3ceeed087c
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-05 17:05:47 +0000

    Updated outputs-datas.tf

--- R-SelfHostedAgents_LinuxVM/outputs-datas.tf
+++ R-SelfHostedAgents_LinuxVM/outputs-datas.tf
@@ -28,16 +28,19 @@ output "key_vault_name" {
 }
 
 output "key_vault_secret_name" {
+  sensitive = true‌
   value       = azurerm_key_vault_secret.vm_admin_password.name
   description = "Name of the secret storing VM password"
 }
 
 output "ssh_connection_command" {
+  sensitive = true‌
   value       = "ssh ${var.admin_username}@${data.azurerm_public_ip.vm_ip.ip_address}"
   description = "SSH command to connect to VM"
 }
 
 output "key_vault_password_retrieval" {
+  sensitive = true‌
   value       = "az keyvault secret show --vault-name ${azurerm_key_vault.main.name} --name vm-admin-password --query value -o tsv"
   description = "Command to retrieve VM password from Key Vault"
   sensitive   = true
