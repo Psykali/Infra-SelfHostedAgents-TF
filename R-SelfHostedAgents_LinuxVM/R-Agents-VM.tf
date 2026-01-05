e72b9b62d520fd5e9412a1db7028479516c51d03
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-05 15:30:48 +0000

    Updated R-Agents-VM.tf

--- R-SelfHostedAgents_LinuxVM/R-Agents-VM.tf
+++ R-SelfHostedAgents_LinuxVM/R-Agents-VM.tf
@@ -2,7 +2,7 @@
 # VIRTUAL MACHINE - DEVOPS AGENTS
 # =============================================
 # Purpose: Create Ubuntu VM for hosting DevOps agents
-# Usage: Self-hosted agent VM with Key Vault integration
+# Usage: Self-hosted agent VM with Key Vault integration via extension
 
 resource "azurerm_linux_virtual_machine" "main" {
   name                = local.vm_name
@@ -10,7 +10,9 @@ resource "azurerm_linux_virtual_machine" "main" {
   location            = azurerm_resource_group.agent.location
   size                = var.vm_size
   admin_username      = var.admin_username
-  admin_password      = azurerm_key_vault_secret.vm_admin_password.value
+  
+  # Use temporary password, will be replaced by Key Vault extension
+  admin_password                  = "TempPassword123!"  # Temporary, will be changed
   disable_password_authentication = false
   
   network_interface_ids = [
@@ -32,21 +34,46 @@ resource "azurerm_linux_virtual_machine" "main" {
     version   = "latest"
   }
   
-  # System-assigned identity for Key Vault access
+  # Both system-assigned and user-assigned identity
   identity {
     type = "SystemAssigned"
   }
-  
-  # Custom data script for agent installation
-  custom_data = base64encode(templatefile("${path.module}/cloud-init.yaml", {
-    storage_account_name = "st${var.client_name}devops${var.environment}${var.location_code}01"
-    storage_account_key  = ""  # Will be injected via Key Vault
-    private_endpoint_ip  = ""  # Will be injected after storage deployment
-  }))
   
   tags = merge(local.common_tags, {
     Component   = "compute"
     Description = "Ubuntu VM for Azure DevOps self-hosted agents"
     OS          = "Ubuntu-22.04-LTS"
+  })
+  
+  # Ensure VM is created before Key Vault extension
+  depends_on = [
+    azurerm_key_vault.main,
+    azurerm_key_vault_secret.vm_admin_password
+  ]
+}
+
+# Use VM extension to retrieve password from Key Vault
+resource "azurerm_virtual_machine_extension" "keyvault" {
+  name                 = "KeyVaultPasswordSetup"
+  virtual_machine_id   = azurerm_linux_virtual_machine.main.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.1"
+  
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "echo 'Password management via Key Vault extension would go here' > /tmp/keyvault-setup.txt"
+    }
+  SETTINGS
+  
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+      "secretUrl": "${azurerm_key_vault_secret.vm_admin_password.id}"
+    }
+  PROTECTED_SETTINGS
+  
+  tags = merge(local.common_tags, {
+    Component = "security"
+    Purpose   = "keyvault-password-setup"
   })
 }
