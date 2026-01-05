f9ef44dae6895a9c2872f27c252a97961c6f6c08
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-05 17:08:14 +0000

    Updated outputs-datas.tf

--- R-SelfHostedAgents_LinuxVM/outputs-datas.tf
+++ R-SelfHostedAgents_LinuxVM/outputs-datas.tf
@@ -32,13 +32,34 @@ output "key_vault_secret_name" {
   description = "Name of the secret storing VM password"
 }
 
+# FIXED: Mark as sensitive since it contains username
 output "ssh_connection_command" {
   value       = "ssh ${var.admin_username}@${data.azurerm_public_ip.vm_ip.ip_address}"
   description = "SSH command to connect to VM"
+  sensitive   = true  
 }
 
+# FIXED: Already marked sensitive
 output "key_vault_password_retrieval" {
   value       = "az keyvault secret show --vault-name ${azurerm_key_vault.main.name} --name vm-admin-password --query value -o tsv"
   description = "Command to retrieve VM password from Key Vault"
   sensitive   = true
+}
+
+# Add non-sensitive useful outputs
+output "deployment_summary" {
+  value = <<EOT
+✅ DevOps Agents Infrastructure Deployed Successfully!
+
+Resources Created:
+- VM: ${azurerm_linux_virtual_machine.main.name}
+- Key Vault: ${azurerm_key_vault.main.name}
+- Network: ${azurerm_virtual_network.main.name}
+
+Next Steps:
+1. Retrieve VM password from Key Vault
+2. Connect to VM using SSH
+3. Monitor agent setup in setup.log on VM
+EOT
+  description = "Deployment summary and next steps"
 }
