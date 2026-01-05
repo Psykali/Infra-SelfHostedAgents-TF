9e773d27836031e91e4b99b2b3cd4a6212ebba0f
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-05 08:39:07 +0000

    Updated providers.tf

--- R-SelfHostedAgents_LinuxVM/providers.tf
+++ R-SelfHostedAgents_LinuxVM/providers.tf
@@ -1,16 +1,28 @@
-### ----------
-### Providers
-### ----------
+### ==========================
+### PROVIDERS CONFIGURATION
+### ==========================
+
 terraform {
   required_version = ">= 1.0"
+  }
+  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
+# Azure provider with features configuration
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
 }
