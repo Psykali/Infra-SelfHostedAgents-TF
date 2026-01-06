9c9ddb94b5815d6e404cfb9f64488c9ad80a150c
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-06 11:03:15 +0000

    Updated R-PrivateEndPoint.tf

--- R-PrivateStorage/R-PrivateEndPoint.tf
+++ R-PrivateStorage/R-PrivateEndPoint.tf
@@ -8,22 +8,27 @@ resource "azurerm_private_endpoint" "storage" {
   name                = local.private_endpoint_name
   location            = azurerm_resource_group.storage.location
   resource_group_name = azurerm_resource_group.storage.name
-  subnet_id           = data.azurerm_subnet.agents.id
-  
+  subnet_id           = azurerm_subnet.private_endpoint.id
+
   private_service_connection {
-    name                           = "${local.storage_account_name}-connection"
+    name                           = local.private_endpoint_connection_name
     private_connection_resource_id = azurerm_storage_account.private.id
     subresource_names              = ["blob"]
     is_manual_connection           = false
   }
+}
+
+### ------------------------------------------------
+### Dedicated subnet for private endpoints (no DNS)
+### ------------------------------------------------
+resource "azurerm_subnet" "private_endpoint" {
+  name                 = local.private_endpoint_subnet_name
+  resource_group_name  = local.networking_rg_name
+  virtual_network_name = local.vnet_name
+  address_prefixes     = ["10.0.0.32/27"]
+
+  service_endpoints = ["Microsoft.Storage"]
   
-  tags = merge(local.common_tags, {
-    Component = "networking"
-    Service   = "storage"
-  })
-  
-  depends_on = [
-    azurerm_storage_account.private,
-    azurerm_storage_container.tfstate
-  ]
+  # Allow private-endpoint policies to be applied
+  private_endpoint_network_policies = "Enabled"
 }
