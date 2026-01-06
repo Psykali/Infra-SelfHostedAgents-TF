5ad21af25677c84c95bffe46dee94c046cdbe874
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-06 14:39:09 +0000

    Updated R-Vnet-Subnet.tf

--- R-SelfHostedAgents_LinuxVM/R-Vnet-Subnet.tf
+++ R-SelfHostedAgents_LinuxVM/R-Vnet-Subnet.tf
@@ -8,8 +8,8 @@
 resource "azurerm_virtual_network" "main" {
   name                = local.vnet_name
   address_space       = ["10.0.0.0/24"]
-  location            = azurerm_resource_group.network.location
-  resource_group_name = azurerm_resource_group.network.name
+  location            = azurerm_resource_group.network_rg.location
+  resource_group_name = azurerm_resource_group.network_rg.name
   
   tags = merge(local.common_tags, {
     Component   = "networking"
@@ -20,7 +20,7 @@ resource "azurerm_virtual_network" "main" {
 # Subnet for DevOps Agents
 resource "azurerm_subnet" "main" {
   name                 = local.subnet_name
-  resource_group_name  = azurerm_resource_group.network.name
+  resource_group_name  = azurerm_resource_group.network_rg.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.0.0/27"]
   
