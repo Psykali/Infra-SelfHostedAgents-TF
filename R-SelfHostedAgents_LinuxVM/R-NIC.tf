ef95fcaf022899e2bfdf06ef851155f2d681bcce
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-06 11:08:54 +0000

    Updated R-NIC.tf

--- R-SelfHostedAgents_LinuxVM/R-NIC.tf
+++ R-SelfHostedAgents_LinuxVM/R-NIC.tf
@@ -6,19 +6,18 @@
 
 resource "azurerm_network_interface" "main" {
   name                = local.nic_name
-  location            = azurerm_resource_group.agent.location
-  resource_group_name = azurerm_resource_group.agent.name
-  
+  location            = azurerm_resource_group.network_rg.location
+  resource_group_name = azurerm_resource_group.network_rg.name
+
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.main.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
   }
-  
+
   tags = merge(local.common_tags, {
-    Component   = "networking"
     Description = "Network interface for DevOps agent VM"
-    AssociatedVM = local.vm_name
+    Component   = "networking"
   })
 }
