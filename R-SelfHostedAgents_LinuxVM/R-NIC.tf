10c01c86f69c9fbd8b2c60c6d6a66e9924a7f762
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-06 14:37:57 +0000

    Updated R-NIC.tf

--- R-SelfHostedAgents_LinuxVM/R-NIC.tf
+++ R-SelfHostedAgents_LinuxVM/R-NIC.tf
@@ -6,8 +6,8 @@
 
 resource "azurerm_network_interface" "main" {
   name                = local.nic_name
-  location            = azurerm_resource_group.network_rg.location
-  resource_group_name = azurerm_resource_group.network_rg.name
+  location            = azurerm_resource_group.vm_rg.location
+  resource_group_name = azurerm_resource_group.vm_rg.name
 
   ip_configuration {
     name                          = "internal"
