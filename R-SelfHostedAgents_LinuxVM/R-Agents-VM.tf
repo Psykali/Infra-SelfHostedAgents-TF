8a2ba4bee3a9f01c024490a5e4aa3cd644062414
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-06 11:08:36 +0000

    Updated R-Agents-VM.tf

--- R-SelfHostedAgents_LinuxVM/R-Agents-VM.tf
+++ R-SelfHostedAgents_LinuxVM/R-Agents-VM.tf
@@ -4,50 +4,41 @@
 # Purpose: Create Ubuntu VM for hosting DevOps agents
 # Usage: Self-hosted agent VM - ALL CONFIGURATION DONE IN Conf-agentVM.tf
 
-# Generate local password for VM
-resource "random_password" "local_vm_password" {
-  length           = 16
-  special          = true
-  override_special = "!@#$%^&*()-_=+[]{}<>:?"
-}
-
-# Create VM with local password
 resource "azurerm_linux_virtual_machine" "main" {
   name                = local.vm_name
-  resource_group_name = azurerm_resource_group.agent.name
-  location            = azurerm_resource_group.agent.location
+  resource_group_name = azurerm_resource_group.vm_rg.name
+  location            = azurerm_resource_group.vm_rg.location
   size                = var.vm_size
   admin_username      = var.admin_username
-  admin_password      = random_password.local_vm_password.result
+  admin_password      = var.admin_password
   disable_password_authentication = false
-  
   network_interface_ids = [
     azurerm_network_interface.main.id,
   ]
-  
-  # OS Disk with custom name
+
   os_disk {
-    name                 = local.os_disk_name
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  
-  # Ubuntu 22.04 LTS
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
-  
-  # System-assigned identity for Key Vault access
+
   identity {
     type = "SystemAssigned"
   }
-  
+
   tags = merge(local.common_tags, {
-    Component   = "compute"
-    Description = "Ubuntu VM for Azure DevOps self-hosted agents"
-    OS          = "Ubuntu-22.04-LTS"
+    Description     = "Ubuntu VM hosting Azure DevOps self-hosted agents"
+    Component       = "compute"
+    OS              = "Ubuntu-22.04-LTS"
+    VM-Size         = var.vm_size
+    AutoPatch       = "false"
+    Backup          = "false"
+    Monitoring      = "basic"
   })
 }
