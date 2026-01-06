5c5505ef97198ed675688bf1e220a5de6f2d84f9
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-06 15:54:22 +0000

    Updated variables-locals.tf

--- R-SelfHostedAgents_LinuxVM/variables-locals.tf
+++ R-SelfHostedAgents_LinuxVM/variables-locals.tf
@@ -71,7 +71,7 @@ locals {
   pip_name     = "pip-${local.vm_name}"
   
   # Key Vault - 
-  kv_name = "kv1-${var.client_name}-${local.workload_name}-${var.environment}-${var.location_code}-${local.sequence_number}"
+  kv_name = "kv-01-${var.client_name}-${local.workload_name}-${var.environment}-${var.location_code}-${local.sequence_number}"
   
   # Storage Account (for Terraform state) 
   storage_name = "st${replace(var.client_name, "-", "")}${local.workload_name}${substr(var.environment, 0, 3)}${var.location_code}${local.sequence_number}"
