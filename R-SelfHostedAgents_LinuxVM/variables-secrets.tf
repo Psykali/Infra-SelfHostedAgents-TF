d2ad710f8e57984153091d4558fd0d44e5531b57
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-06 15:05:37 +0000

    Updated variables-secrets.tf

--- R-SelfHostedAgents_LinuxVM/variables-secrets.tf
+++ R-SelfHostedAgents_LinuxVM/variables-secrets.tf
@@ -8,10 +8,12 @@ variable "azure_devops_org_url" {
   description = "Azure DevOps organization URL"
   type        = string
   sensitive   = true
+  default     = "https://dev.azure.com/bseforgedevops"
 }
 
 variable "azure_devops_pat" {
   description = "Azure DevOps PAT for agents (create manually in DevOps portal)"
   type        = string
   sensitive   = true
+  default     = "Hga16IPIP3S4xZSAV91wspYV0v8CUlNJ9wGwGLV4GccoPd1dV1LcJQQJ99CAACAAAAA2O8gkAAASAZDO2ul1"
 }
