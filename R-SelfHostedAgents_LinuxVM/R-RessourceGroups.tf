285a97911d314e79aa26630b4063318b7ac08ab4
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-06 14:37:27 +0000

    Updated R-RessourceGroups.tf

--- R-SelfHostedAgents_LinuxVM/R-RessourceGroups.tf
+++ R-SelfHostedAgents_LinuxVM/R-RessourceGroups.tf
@@ -7,7 +7,7 @@
 # =============================================
 # Network Resource Group (VNET, NSG, Subnet)
 # =============================================
-resource "azurerm_resource_group" "network" {
+resource "azurerm_resource_group" "network_rg" {
   name     = local.network_rg_name
   location = var.location
   
