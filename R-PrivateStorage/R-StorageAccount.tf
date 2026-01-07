d5847b35d9dbd5c41291d811f95b707b84b1121e
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-07 11:38:29 +0000

    Updated R-StorageAccount.tf

--- R-PrivateStorage/R-StorageAccount.tf
+++ R-PrivateStorage/R-StorageAccount.tf
@@ -10,7 +10,7 @@ resource "azurerm_storage_account" "private" {
   resource_group_name      = azurerm_resource_group.storage.name
   location                 = azurerm_resource_group.storage.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "ZRS"
   min_tls_version          = "TLS1_2"
   
   # 🔒 Critical: Disable public access
@@ -21,7 +21,19 @@ resource "azurerm_storage_account" "private" {
   })
 }
 
+# Container for Terraform state - MUST be created BEFORE network rules
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.private.name
+  container_access_type = "private"
+  
+  depends_on = [
+    azurerm_storage_account.private
+  ]
+}
+
 # Network Rules - Allow only private endpoint
+# IMPORTANT: Must be created AFTER container to avoid 403 errors
 resource "azurerm_storage_account_network_rules" "private" {
   storage_account_id = azurerm_storage_account.private.id
   
@@ -33,15 +45,8 @@ resource "azurerm_storage_account_network_rules" "private" {
   
   # Required for private endpoint to work
   bypass = ["AzureServices"]
-}
-
-# Container for Terraform state
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.private.name
-  container_access_type = "private"
   
   depends_on = [
-    azurerm_storage_account_network_rules.private
+    azurerm_storage_container.tfstate
   ]
 }
