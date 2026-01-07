d018fdd958bec79610f3c2463fd898ef77e51052
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-07 11:25:04 +0000

    Updated R-RessourceGroup.tf

--- R-PrivateStorage/R-RessourceGroup.tf
+++ R-PrivateStorage/R-RessourceGroup.tf
@@ -7,14 +7,5 @@
 resource "azurerm_resource_group" "storage" {
   name     = local.storage_rg_name
   location = var.location
-  
-  tags = merge(local.common_tags, {
-    Description = "Persistent resource group for Terraform state storage"
-    CanDelete   = "false"  # Warning: Contains Terraform state!
-  })
-  
-  # Prevent accidental deletion
-  lifecycle {
-    prevent_destroy = true
-  }
+  tags     = local.common_tags
 }
