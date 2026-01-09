c6dd53b68e7b4156f6a99d0cda8edbf4cf9ae43a
Siefeleslam KHALIFA <skhalifa@bseforge.onmicrosoft.com>
2026-01-09 08:29:19 +0000

    Updated R-NIC.tf

--- R-SelfHostedAgents_LinuxVM/R-NIC.tf
+++ R-SelfHostedAgents_LinuxVM/R-NIC.tf
@@ -24,5 +24,5 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_network_interface_security_group_association" "vm_nic_nsg" {
   network_interface_id      = azurerm_network_interface.main.id
-  network_security_group_id = azurerm_network_security_group.agents_nsg.id
+  network_security_group_id = azurerm_network_security_group.network_rg.id
 }
